8c9ba8bcdf3ef4692ce66e93caea7cef540489ef
puripuri2100 <puripuri2100@gmail.com>
2024-05-28 13:46:14 +0900

    update

--- week5/src/nature.rs
+++ week5/src/nature.rs
@@ -10,13 +10,22 @@ pub fn main() {
     let m = s_l.next().unwrap().parse::<usize>().unwrap();
     let l = s_l.next().unwrap().parse::<usize>().unwrap();
     let _s = s_l.next().unwrap().parse::<usize>().unwrap();
-    let mut start = Vec::new();
-    let mut stations = vec![vec![]; n + 1];
+    let mut root_info = vec![None; n + 1];
+    let mut childrens = vec![Vec::new(); n + 1];
+    let mut links = Vec::new();
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
-    for s in s.split_whitespace() {
+    let mut root = 0;
+    for (i, s) in s.split_whitespace().enumerate() {
       let n = s.parse::<usize>().unwrap();
-      start.push(n)
+      if i == 0 {
+        root_info[n] = Some(n);
+        root = n;
+        childrens[n].push(n);
+      } else {
+        root_info[n] = Some(root);
+        childrens[root].push(n);
+      }
     }
     for _ in 0..m {
       s = String::new();
@@ -25,56 +34,47 @@ pub fn main() {
       let i = s_l.next().unwrap().parse::<usize>().unwrap();
       let j = s_l.next().unwrap().parse::<usize>().unwrap();
       let e = s_l.next().unwrap().parse::<usize>().unwrap();
-      stations[i].push((j, e));
-      stations[j].push((i, e))
+      links.push((i, j, e + l));
     }
-
-    let mut coast = vec![None; n + 1];
-    let mut queue = std::collections::VecDeque::new();
-    for s in start {
-      coast[s] = Some((s, 0_usize));
-      for (n, e_ij) in &stations[s] {
-        if let Some((_, e)) = coast[*n] {
-          if e > e_ij + l {
-            // 小さい値で更新
-            coast[*n] = Some((s, e_ij + l));
-            queue.push_back(n);
-          }
-        } else {
-          coast[*n] = Some((s, e_ij + l));
-          queue.push_back(n);
-        }
-      }
-    }
-
-    while let Some(n1) = queue.pop_front() {
-      for (n2, e_ij) in &stations[*n1] {
-        // 親側の値は更新してはいけない
-        // n1：値が更新されたマス
-        // s：n1の親のマス
-        // s2：sの親のマス
-        // n2：n1に依存するマス
-        if let Some((s, e)) = coast[*n2] {
-          // 循環参照にならないようにするチェック
-          let is_ok = if let Some((s2, _)) = coast[*n1] {
-            s2 != *n2
-          } else {
-            true
-          };
-          if is_ok && s != *n2 && e > e_ij + l {
-            coast[*n2] = Some((*n1, e_ij + l));
-            queue.push_back(n2);
-          }
-        } else {
-          coast[*n2] = Some((*n1, e_ij + l));
-          queue.push_back(n2);
-        }
-      }
-    }
+    links.sort_by_key(|(_, _, e)| *e);
 
     let mut ans = 0;
-    for (_, e) in coast.iter().flatten() {
-      ans += e;
+    for (i, j, e) in links.iter() {
+      match (root_info[*i], root_info[*j]) {
+        (None, None) => {
+          // 新しくリンクを登録する
+          root_info[*i] = Some(*i);
+          root_info[*j] = Some(*i);
+          childrens[*i].push(*i);
+          childrens[*i].push(*j);
+          ans += e;
+        }
+        (Some(root1), None) => {
+          // すでに片方がグラフに繋がってるのでそこに接続する
+          root_info[*j] = Some(root1);
+          childrens[root1].push(*j);
+          ans += e;
+        }
+        (None, Some(root1)) => {
+          // すでに片方がグラフに繋がってるのでそこに接続する
+          root_info[*i] = Some(root1);
+          childrens[root1].push(*i);
+          ans += e;
+        }
+        (Some(root1), Some(root2)) => {
+          // rootが一致していたらすでにより小さいコストで繋がっているので何もしない
+          if root1 != root2 {
+            // 片方にもう片方を結合させる
+            let children2 = childrens[root2].clone();
+            for n in children2.iter() {
+              root_info[*n] = Some(root1);
+              childrens[root1].push(*n);
+            }
+            childrens[root2] = Vec::new();
+            ans += e;
+          }
+        }
+      }
     }
     println!("{ans}");
   }
